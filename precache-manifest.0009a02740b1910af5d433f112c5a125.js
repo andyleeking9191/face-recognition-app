7b275ac9be4b43da3376104246c308cfebba2991
andyleeking9191 <andyleeking9191@users.noreply.github.com>
2020-03-03 14:49:46 -0500

    Updates

---
+++ precache-manifest.0009a02740b1910af5d433f112c5a125.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7b784d3c87540742b015a8b19c2e894a",
+    "url": "/face-recognition-app/index.html"
+  },
+  {
+    "revision": "4910509fc15eddb9a3d1",
+    "url": "/face-recognition-app/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "1567e2c66228f3740786",
+    "url": "/face-recognition-app/static/css/main.98ad8409.chunk.css"
+  },
+  {
+    "revision": "4910509fc15eddb9a3d1",
+    "url": "/face-recognition-app/static/js/2.40312223.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/face-recognition-app/static/js/2.40312223.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1567e2c66228f3740786",
+    "url": "/face-recognition-app/static/js/main.96cf277e.chunk.js"
+  },
+  {
+    "revision": "1e696ea1ee776e02588f",
+    "url": "/face-recognition-app/static/js/runtime-main.8eb6cffe.js"
+  }
+]);
